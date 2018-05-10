eebf20a8b2448c085fb04e060a349dbca801a583
fsociety.1 <fsociety.1@fsociety1.local>
2018-05-10 20:15:52 +0600

    Initial Commit

---
+++ AppFor4S/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppFor4S
+//
+//  Created by fsociety.1 on 5/10/18.
+//  Copyright © 2018 fsociety.1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
